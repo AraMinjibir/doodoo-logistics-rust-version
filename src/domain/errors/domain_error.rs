aff9ed340e7bcf6ccad9f0ae49ca9c9c186151db
AraMinjibir <abubakar.aramjb@gmail.com>
2026-04-27 19:13:03 +0100

    refactor(validation error): new error feature added

--- src/domain/errors/domain_error.rs
+++ src/domain/errors/domain_error.rs
@@ -6,6 +6,7 @@ pub enum DomainError {
     DuplicateProofOfDelivery,
     UpdateProofOfDeliveryError(String),
     SubmittedByEmptyError,
+    ValidationError(Vec<String>),
 }
 
 impl fmt::Display for DomainError {
@@ -21,7 +22,11 @@ impl fmt::Display for DomainError {
                 write!(f, "Unable to update the proof of delivery: {}", cause),
 
             DomainError::SubmittedByEmptyError  =>
-            write!(f, "Unable to fecth the proof's sender details"),    
+            write!(f, "Unable to fecth the proof's sender details"),   
+            DomainError::ValidationError(causes) =>
+                write!(f, "Validation failed: {}", causes.join(", ")),
+                
+ 
         }
     }
 }
