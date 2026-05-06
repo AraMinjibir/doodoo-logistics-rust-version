bee488459818cfb4fc6cb3a8b92ea3877d9a7f27
AraMinjibir <abubakar.aramjb@gmail.com>
2026-05-06 17:42:21 +0100

    refactor: clean up and organize common test utilities
    Streamlined test database management and fixture generation logic in
    the common test module. Removed unused imports and applied consistent
    formatting to ensure a warning-free and maintainable test suite.

--- src/tests/common/db.rs
+++ src/tests/common/db.rs
@@ -1,7 +1,19 @@
+#![allow(dead_code)] // This tells Rust to only look at this folder during 'cargo test'
+
 use sqlx::{PgPool, Executor, postgres::PgPoolOptions};
+use actix_web::dev::{Service, ServiceResponse};
+use actix_http::Request;
 use std::env;
 use dotenvy::dotenv;
 use std::time::Duration;
+use testcontainers::{clients::Cli, Container};
+use testcontainers_modules::postgres::Postgres;
+use tokio::time::sleep;
+use std::sync::OnceLock;
+static DOCKER: OnceLock<Cli> = OnceLock::new();
+
+use crate::tests::common::test_app::setup_app_with_pool;
+
 
 pub struct TestDb {
     pub pool: PgPool,
@@ -46,5 +58,65 @@ impl TestDb {
         pool.execute("TRUNCATE TABLE shipments RESTART IDENTITY CASCADE")
             .await
             .unwrap();
+    }
+}
+
+pub struct TestContainerDb<'a> {
+    pub container: Container<'a, Postgres>,
+    pub connection_string: String,
+}
+
+pub async fn setup_test_db<'a>(docker: &'a Cli) -> TestContainerDb<'a> {
+    let container = docker.run(Postgres::default());
+
+    let port = container.get_host_port_ipv4(5432);
+
+    let connection_string = format!(
+        "postgres://postgres:postgres@127.0.0.1:{}/postgres",
+        port
+    );
+
+    TestContainerDb {
+        container,
+        connection_string,
+    }
+}
+
+pub async fn wait_for_db(connection_string: &str) -> PgPool {
+    for _ in 0..10 {
+        match PgPool::connect(connection_string).await {
+            Ok(pool) => return pool,
+            Err(_) => sleep(Duration::from_secs(1)).await,
+        }
+    }
+
+    panic!("Database not ready");
+}
+
+pub struct TestContext<S> {
+    pub app: S,
+    pub pool: PgPool,
+    pub _container: Container<'static, Postgres>,
+}
+
+pub async fn spawn_app() -> TestContext<impl Service<Request, Response = ServiceResponse, Error = actix_web::Error>> {
+    // 1. Initialize Docker and DB
+    let docker = DOCKER.get_or_init(Cli::default);
+    let container = docker.run(Postgres::default());
+
+    let port = container.get_host_port_ipv4(5432);
+
+    let connection_string = format!("postgres://postgres:postgres@127.0.0.1:{}/postgres", port);
+    
+    // 2. Connect and Migrate
+    let pool = wait_for_db(&connection_string).await;
+    sqlx::migrate!("./migrations").run(&pool).await.expect("Migrations failed");
+    // 3. Build the Actix App
+    let app = setup_app_with_pool(pool.clone()).await;
+
+    TestContext { 
+        app, 
+        pool, 
+        _container: container 
     }
 }
