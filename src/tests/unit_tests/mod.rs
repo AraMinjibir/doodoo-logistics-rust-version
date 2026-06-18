edecb2472160ce53512c010514495c082b682adc
AraMinjibir <abubakar.aramjb@gmail.com>
2026-06-18 11:41:22 +0100

    chore(support): wire support unit test

--- src/tests/unit_tests/mod.rs
+++ src/tests/unit_tests/mod.rs
@@ -1,4 +1,6 @@
-#[cfg(test)]
+#[cfg(test)]  // This tells Rust to only look at this folder during 'cargo test'
 pub mod payment_unit_test;
-#[cfg(test)] // This tells Rust to only look at this folder during 'cargo test'
+#[cfg(test)]
 pub mod shipment_unit_test;
+#[cfg(test)]
+pub mod support_unit_test;
