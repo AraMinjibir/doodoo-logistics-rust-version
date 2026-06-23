78a9c31fc0d0984790d5127c8750102e771f0d09
AraMinjibir <abubakar.aramjb@gmail.com>
2026-06-23 15:47:04 +0100

    chore(user) register repository module

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -3,4 +3,6 @@ pub mod shipment_repository;
 pub mod sqlx_payment_repository;
 pub mod sqlx_shipment_repository;
 pub mod sqlx_support_repository;
+pub mod sqlx_user_repository;
 pub mod support_repository;
+pub mod user_repository;
